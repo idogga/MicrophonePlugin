77c4711446023d9f65d25ba8ef6b07c76163a939
sir Lenko <lenko.studio@gmail.com>
2018-12-02 17:29:42 +0700

    свойства модели

--- MicrophomePlugin/CadConnect.cs
+++ MicrophomePlugin/CadConnect.cs
@@ -1,13 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MicrophonePlugin
 {
+    /// <summary>
+    /// Хранение соединения с Кад-системой
+    /// </summary>
     public class CadConnect : IDisposable
     {
+        /// <summary>
+        /// Очистка данных
+        /// </summary>
         public void Dispose()
         {
             throw new NotImplementedException();
--- MicrophomePlugin/MicroVM.cs
+++ MicrophomePlugin/MicroVM.cs
@@ -1,9 +1,67 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 
 namespace MicrophonePlugin
 {
+    /// <summary>
+    /// Модель микрофона
+    /// </summary>
     public class MicroVM : BindableBase
     {
+        private double _totalLenght = 80;
+        private double _capsuleRadius = 80;
+        private double _handleDiametr = 80;
+        private double _handleLenght = 80;
+        private double _clipLenght = 80;
+
+        /// <summary>
+        /// Построение можели
+        /// </summary>
+        public DelegateCommand Build;
+
+        /// <summary>
+        /// Установка значений по-умолчанию
+        /// </summary>
+        public DelegateCommand MakeDefault;
+        
+        private void PropertyChange() { }
+
+        /// <summary>
+        /// Общая длина микрофона
+        /// </summary>
+        public string TotalLenght
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Радиус капсюли
+        /// </summary>
+        /// <remarks>
+        /// набалдшника
+        /// </remarks>
+        public string CapsuleRadius { get; set; }
+
+        /// <summary>
+        /// диаметр ручки
+        /// </summary>
+        public string HandleDiametr { get; set; }
+
+        /// <summary>
+        /// длина ручки
+        /// </summary>
+        public string HandleLenght { get; set; }
+
+        /// <summary>
+        /// Длина задвижки
+        /// </summary>
+        public string ClipLenght { get; set; }
+
+        /// <summary>
+        /// Доступность построения
+        /// </summary>
+        public bool IsEnableBuild { get; set; }
 
     }
 }
